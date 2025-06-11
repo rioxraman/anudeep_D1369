5413e2bfbbc70bca74fe49ad06d080f54cd50430
rioxraman <ramanpkgupta@gmail.com>
2025-06-11 11:28:41 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; //1  impi
+
+public class Main { // main
+
+    public static void main(String[] args) {
+
+       int number= 0;
+       if(number%2==0){
+           System.out.println("You are even number");
+       }else {
+           System.out.println("You are not even number");
+       }
+
+    }
+}
+
+//x=13
